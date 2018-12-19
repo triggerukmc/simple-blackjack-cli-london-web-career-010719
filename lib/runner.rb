b0d1fc9c9a46c90f33dfde6465146d2b06b6e32e
Matthew Collins <matthew.collins1991@yahoo.com>
2018-12-19 17:48:39 +0000

    Automatically backed up by Learn

--- lib/runner.rb
+++ lib/runner.rb
@@ -1,16 +1,5 @@
 require_relative "blackjack.rb"
 
-def runner(total, card_total, card_total2)
-
-welcome
-
-initial_round
-
-hit?(total) until display_cards_total(card_total) >21
-
-end_game
-
-end
-
+runner
 
   
