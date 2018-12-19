aaee5b92a84478cddad20da8a4abe0799bbd27ec
Matthew Collins <matthew.collins1991@yahoo.com>
2018-12-19 17:48:45 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -55,6 +55,14 @@ end
 #####################################################
 
 def runner
-  # code runner here
+   welcome
+  total = initial_round
+  until total > 21
+  total = hit?(total)
+  display_card_total(total)
+end
+
+end_game(total)
+end
 end
     
